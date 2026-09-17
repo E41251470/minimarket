6a857606cf1f89c0be6bdd499c20ef5ac73cc8e2
E41251470 <e41251470@student.polije.ac.id>
2026-09-18 02:48:03 +0700

    Menambahkan template frontend dan admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\productController;
 use Illuminate\Support\Facades\Route;
 
+// Halaman Frontend
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('frontend.home');
+})->name('home');
+
+// Halaman Login
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+// Halaman Admin
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+
+// Route Product
+Route::get('/posts', [productController::class, 'index']);
